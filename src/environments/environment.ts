0779c9e0056e0969bd5eacd6661a661db5a0ff1d
EdwinMateria <127867809+EdwinMateria@users.noreply.github.com>
2023-07-11 16:29:46 -0500

    config publicado con api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,7 @@
 
 export const environment = {
   production: true,
-  siniestroAPI: 'http://localhost:30897/Api',
-  reservaApi: 'http://localhost:30897/Api',
-  kunturapi: 'http://localhost:30897/Api',
+  siniestroAPI: 'https://plataformadigital.stg.protectasecurity.pe/SiniestroSoatApi/Api'
 };
 
 /*
