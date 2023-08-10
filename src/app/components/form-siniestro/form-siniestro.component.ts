cbf3619dd1b288770cb5730b3c5da4cea252ff7e
EdwinMateria <127867809+EdwinMateria@users.noreply.github.com>
2023-08-10 17:49:36 -0500

    correcciones - vista formulario / Validacion fecha siniestro

--- src/app/components/form-siniestro/form-siniestro.component.ts
+++ src/app/components/form-siniestro/form-siniestro.component.ts
@@ -13,9 +13,9 @@ import { ClaimBeneficiarioRequest } from 'src/app/core/models/claimBeneficiarioR
 import { ReserveService } from 'src/app/core/services/reserve/reserve.service';
 
 
-export class SiniestroSelect{
-  codigo    : string;
-  descript  : string;
+export class SiniestroSelect {
+  codigo: string;
+  descript: string;
 }
 
 @Component({
@@ -26,73 +26,70 @@ export class SiniestroSelect{
 export class FormSiniestroComponent implements OnInit {
 
   @Output() cancelBool = new EventEmitter<boolean>();
-  @Input() estadoForm : number;
-  @Input() casoBM : CasosBM = new CasosBM();
+  @Input() estadoForm: number;
+  @Input() casoBM: CasosBM = new CasosBM();
   eliminado = false;
   form!: FormGroup;
   eliminarSiniestro = '0';
   fechaMap = "";
   nSiniestro = "";
   sCliente = "";
-  fechaRechazo : any;
+  fechaRechazo: any;
   moneda: number;
   //Tipos Ocupantes:
-  ocupantes : SiniestroSelect[] = [
-    {codigo: "", descript: "SELECCIONAR"},
-    {codigo : "1", descript : "Si"},
-    {codigo : "2", descript : "No"}
-  ]
-  
-  // Tipos Atencion:
-  atenciones : SiniestroSelect[] = [
-    {codigo: "", descript: "SELECCIONAR"},
-    {codigo: "A", descript: "Ambulatorio"},
-    {codigo: "H", descript: "Hospitalario"},
-    {codigo: "E", descript: "Emergencia"}
+  ocupantes: SiniestroSelect[] = [
+    { codigo: "", descript: "SELECCIONAR" },
+    { codigo: "1", descript: "Si" },
+    { codigo: "2", descript: "No" }
   ]
 
-  constructor(public fb: FormBuilder, private modalService: NgbModal, public casoService: CasosService, private datePipe: DatePipe, 
+  // Tipos Atencion:
+  atenciones: SiniestroSelect[] = [
+    { codigo: "", descript: "SELECCIONAR" },
+    { codigo: "A", descript: "Ambulatorio" },
+    { codigo: "H", descript: "Hospitalario" },
+    { codigo: "E", descript: "Emergencia" }
+  ]
+
+  constructor(public fb: FormBuilder, private modalService: NgbModal, public casoService: CasosService, private datePipe: DatePipe,
     public authProtectaService: AuthProtectaService, public reserveService: ReserveService) {
   }
 
   ngOnInit(): void {
-    console.log(this.casoBM.dFecOcurrencia);
-    console.log(new Date(this.casoBM.dFecOcurrencia))
-    
     this.fechaMap = new Date(this.casoBM.dFecOcurrencia).toLocaleDateString('en-GB')
     this.form = this.fb.group({
-      npolicy: [{value: '', disabled: true}],
-      certif: [{value: '', disabled: true}],
-      ncaso : [{value: '', disabled: true}],
-      nsiniestro : [{value: '', disabled: true}],
-      fOcurrencia: [{value: '', disabled: true}],
-      hOcurrencia: [{value: '', disabled: true}],
-      causaSiniestro: [{value: '', disabled: true}],
-      moneda: [{value: '', disabled: true}],
-      dFecDenuncia : [{value:'', disabled: false}, Validators.required],
-      sHoraRecepcion: [{value:'', disabled: false}, Validators.required],
-      dFecApertura: [{value:'', disabled: true}, Validators.required],
-      afectado: [{value:'', disabled: false}, Validators.required],
-      nTipOcupante : [{value:'', disabled: false}, Validators.required],
-      sTipAtencion: [{value:'', disabled: false}, Validators.required],
-      dFecFallecido: [{value:'', disabled: false}],
-      sEquivSiniestro: [{value:'', disabled: false}],
+      npolicy: [{ value: '', disabled: true }],
+      certif: [{ value: '', disabled: true }],
+      ncaso: [{ value: '', disabled: true }],
+      nsiniestro: [{ value: '', disabled: true }],
+      fOcurrencia: [{ value: '', disabled: true }],
+      hOcurrencia: [{ value: '', disabled: true }],
+      causaSiniestro: [{ value: '', disabled: true }],
+      moneda: [{ value: '', disabled: true }],
+      dFecDenuncia: [{ value: '', disabled: false }, Validators.required],
+      sHoraRecepcion: [{ value: '', disabled: false }, Validators.required],
+      dFecApertura: [{ value: '', disabled: true }, Validators.required],
+      afectado: [{ value: '', disabled: false }, Validators.required],
+      nTipOcupante: [{ value: '', disabled: false }, Validators.required],
+      sTipAtencion: [{ value: '', disabled: false }, Validators.required],
+      dFecFallecido: [{ value: '', disabled: false }],
+      sEquivSiniestro: [{ value: '', disabled: false }],
     })
     this.disabledForm();
     this.moneda = this.casoBM.nMoneda;
     this.form.controls['moneda'].setValue(this.moneda == 1 ? 'SOLES' : (this.moneda == 2 ? 'DOLARES' : ''))
-    if(this.estadoForm == 2){
+    if (this.estadoForm == 2) {
       this.form.controls['dFecApertura'].setValue(this.datePipe.transform(new Date(), 'dd/MM/yyyy'))
     }
-    if(this.estadoForm != 2 ){
+    if (this.estadoForm != 2) {
       let origen = 1;
-      if(this.estadoForm == 3) origen = 2
+      if (this.estadoForm == 3) origen = 2
       //Edicion y rechazo siniestro 
       SwalCarga();
-      this.casoService.GetSearchClaim(this.casoBM.nSiniestro , origen).subscribe(
+      this.casoService.GetSearchClaim(this.casoBM.nSiniestro, origen).subscribe(
         res => {
           Swal.close()
-          if(res.GenericResponse.length == 0){
+          if (res.GenericResponse.length == 0) {
             Swal.fire({
               title: 'Información',
               text: 'El siniestro tiene pago total',
@@ -104,7 +101,7 @@ export class FormSiniestroComponent implements OnInit {
                 this.cancelBool.emit(true)
               }
             })
-          }else{
+          } else {
             let siniestro = new SiniestroBM();
             siniestro = res.GenericResponse[0];
             this.moneda = siniestro.nMoneda;
@@ -118,8 +115,8 @@ export class FormSiniestroComponent implements OnInit {
             this.form.controls['dFecFallecido'].setValue(this.datePipe.transform(siniestro.dFecFallecido, 'yyyy-MM-dd'))
             this.sCliente = siniestro.sCodClie;
             this.form.controls['nTipOcupante'].setValue(siniestro.sTipOcupante);
-  
-            if(siniestro.nCodRechazo != 0 && this.estadoForm == 3){
+
+            if (siniestro.nCodRechazo != 0 && this.estadoForm == 3) {
               this.eliminado = true;
               let fRechazo = this.datePipe.transform(siniestro.dFecRechazo, 'yyyy-MM-dd').split('-');
               let anio = fRechazo[0];
@@ -132,18 +129,18 @@ export class FormSiniestroComponent implements OnInit {
         },
         err => {
           Swal.close();
-          Swal.fire('Error',err,'error')
+          Swal.fire('Error', err, 'error')
         }
       )
     }
   }
 
-  opcionVolver(){
+  opcionVolver() {
     this.cancelBool.emit(true)
   }
 
-  rechazoSiniestro(){
-    if(this.eliminarSiniestro != '0'){
+  rechazoSiniestro() {
+    if (this.eliminarSiniestro != '0') {
       SwalCarga();
       let siniestro = new SiniestroBM();
       siniestro.nCaso = this.casoBM.nCaso;
@@ -156,17 +153,17 @@ export class FormSiniestroComponent implements OnInit {
       siniestro.dFecApertura = this.datePipe.transform(new Date(faper[2], faper[1] - 1, faper[0]), 'yyyy-MM-dd');
 
       let cookie = this.authProtectaService.getCookie('AppSiniestro');
-      let codUsuario = this.authProtectaService.getValueCookie('CodUsu',cookie);
+      let codUsuario = this.authProtectaService.getValueCookie('CodUsu', cookie);
       siniestro.nCodUsuario = Number(atob(codUsuario));
       const data: FormData = new FormData();
       data.append('siniestrosData', JSON.stringify(siniestro));
       this.casoService.AddRechazo(data).subscribe(
         res => {
           Swal.close()
-          if(res.Message != 'Ok'){
-            Swal.fire('Información',res.Message,'error');
+          if (res.Message != 'Ok') {
+            Swal.fire('Información', res.Message, 'error');
             return;
-          }else{
+          } else {
             Swal.fire('Información', 'Siniestro rechazado correctamente', 'success');
             this.eliminado = true;
             let dia = ("0" + (new Date().getDate())).slice(-2);
@@ -178,98 +175,123 @@ export class FormSiniestroComponent implements OnInit {
         },
         err => {
           Swal.close()
-          Swal.fire('Error',err,'error')
+          Swal.fire('Error', err, 'error')
         }
       )
-    }else{
-      Swal.fire('Información','Debe seleccionar un motivo de rechazo', 'warning');
+    } else {
+      Swal.fire('Información', 'Debe seleccionar un motivo de rechazo', 'warning');
     }
   }
 
-  saveSiniestro(){
+  validacionFormularioSiniestro() {
+    let msj = '';
+    if (this.form.controls['dFecDenuncia'].invalid) {
+      msj += 'Debe ingresar la fecha de denuncia.<br/>';
+    }
+    if (this.form.controls['sHoraRecepcion'].invalid) {
+      msj += 'Debe ingresar la hora de recepción. <br/>';
+    }
+    if (this.form.controls['dFecApertura'].invalid) {
+      msj += 'Debe ingresar la fecha de apertura. <br/>';
+    }
+    if (this.form.controls['afectado'].invalid) {
+      msj += 'Debe ingresar al afectado. <br/>';
+    }
+    if (this.form.controls['nTipOcupante'].invalid) {
+      msj += 'Debe ingresar el ocupante. <br/>';
+    }
+    if (this.form.controls['sTipAtencion'].invalid) {
+      msj += 'Debe ingresar el tipo de atención. <br/>';
+    }
+    Swal.fire('Información', msj, 'warning');
+    return;
+  }
+
+  saveSiniestro() {
     if (this.form.invalid) {
       this.form.markAllAsTouched();
-    }else{
-      if(this.estadoForm == 2){
+      this.validacionFormularioSiniestro();
+    } else {
+      if (this.estadoForm == 2) {
         //Creacion siniestro
         let siniestroBM = new SiniestroBM();
         let cookie = this.authProtectaService.getCookie('AppSiniestro');
-      let codUsuario = this.authProtectaService.getValueCookie('CodUsu',cookie);
-      this.form.controls['nsiniestro'].setValue("0");
-      //this.form.controls['dFecApertura'].setValue(this.datePipe.transform(new Date(), 'yyyy-MM-dd'));
-      let faper = (this.form.controls['dFecApertura'].value).split("/")
+        let codUsuario = this.authProtectaService.getValueCookie('CodUsu', cookie);
+        this.form.controls['nsiniestro'].setValue("0");
+        //this.form.controls['dFecApertura'].setValue(this.datePipe.transform(new Date(), 'yyyy-MM-dd'));
+        let faper = (this.form.controls['dFecApertura'].value).split("/")
 
-      siniestroBM = {
-        ...this.form.getRawValue(),
-        nCaso : this.casoBM.nCaso,
-        sCliente : this.sCliente,
-        nCodUsuario : Number(atob(codUsuario)),
-        dFecApertura : (this.datePipe.transform(new Date(faper[2], faper[1] - 1, faper[0]), 'yyyy-MM-dd'))
-      }
-      const data: FormData = new FormData();
-      data.append('siniestrosData', JSON.stringify(siniestroBM));
-      SwalCarga();
-      this.casoService.AddSiniestros(data).subscribe(
-        res => {
-          Swal.close();
-          if(res.Message != "OK"){
-            Swal.fire('Información',res.Message, 'error');
-            return;
-          }else{
-            Swal.fire({
-              title: 'Información',
-              text: `Se declaró el siniestro ${res.numclaim} correctamente. ¿Desea declarar otro siniestro?`,
-              icon: 'warning',
-              showCancelButton: true,
-              confirmButtonText: 'Sí',
-              cancelButtonText: 'No',
-              reverseButtons: true,
-              showCloseButton: true
-            }).then((result) => {
-              if (result.isConfirmed) {
-                this.form.controls['dFecDenuncia'].setValue("");
-                this.form.controls['sHoraRecepcion'].setValue("");
-                this.form.controls['afectado'].setValue("");
-                this.form.controls['nTipOcupante'].setValue("");
-                this.form.controls['sTipAtencion'].setValue("");
-                this.form.controls['dFecFallecido'].setValue("");
-                this.form.controls['sEquivSiniestro'].setValue("");
-              }else{
-                this.cancelBool.emit(true)
-              }
-            })
-          }
-        },
-        err => {
-          Swal.close();
-          Swal.fire('Error',err,'error')
+        siniestroBM = {
+          ...this.form.getRawValue(),
+          nCaso: this.casoBM.nCaso,
+          sCliente: this.sCliente,
+          nCodUsuario: Number(atob(codUsuario)),
+          dFecApertura: (this.datePipe.transform(new Date(faper[2], faper[1] - 1, faper[0]), 'yyyy-MM-dd'))
         }
-      )
+        const data: FormData = new FormData();
+        data.append('siniestrosData', JSON.stringify(siniestroBM));
+        SwalCarga();
+        this.casoService.AddSiniestros(data).subscribe(
+          res => {
+            Swal.close();
+            if (res.Message != "OK") {
+              Swal.fire('Información', res.Message, 'error');
+              return;
+            } else {
+              Swal.fire({
+                title: 'Información',
+                text: `Se declaró el siniestro ${res.numclaim} correctamente. ¿Desea declarar otro siniestro?`,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sí',
+                cancelButtonText: 'No',
+                reverseButtons: true,
+                showCloseButton: true
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.form.controls['dFecDenuncia'].setValue("");
+                  this.form.controls['sHoraRecepcion'].setValue("");
+                  this.form.controls['afectado'].setValue("");
+                  this.form.controls['nTipOcupante'].setValue("");
+                  this.form.controls['sTipAtencion'].setValue("");
+                  this.form.controls['dFecFallecido'].setValue("");
+                  this.form.controls['sEquivSiniestro'].setValue("");
+                } else {
+                  this.cancelBool.emit(true)
+                }
+              })
+            }
+          },
+          err => {
+            Swal.close();
+            Swal.fire('Error', err, 'error')
+          }
+        )
       }
-      if(this.estadoForm == 1){
+      if (this.estadoForm == 1) {
         //Editar siniestro
         let siniestroBM = new SiniestroBM();
         let cookie = this.authProtectaService.getCookie('AppSiniestro');
-        let codUsuario = this.authProtectaService.getValueCookie('CodUsu',cookie);
+        let codUsuario = this.authProtectaService.getValueCookie('CodUsu', cookie);
         let faper = (this.form.controls['dFecApertura'].value).split("/")
         siniestroBM = {
           ...this.form.getRawValue(),
-          nPolicy : this.casoBM.nPolicy,
-          nCertif : this.casoBM.nCertif,
-          nCaso : this.casoBM.nCaso,
-          nSiniestro : this.nSiniestro,
-          sCliente : this.sCliente,
-          nCodUsuario : Number(atob(codUsuario)),
-          dFecApertura : (this.datePipe.transform(new Date(faper[2], faper[1] - 1, faper[0]), 'yyyy-MM-dd'))
+          nPolicy: this.casoBM.nPolicy,
+          nCertif: this.casoBM.nCertif,
+          nCaso: this.casoBM.nCaso,
+          nSiniestro: this.nSiniestro,
+          sCliente: this.sCliente,
+          nCodUsuario: Number(atob(codUsuario)),
+          dFecApertura: (this.datePipe.transform(new Date(faper[2], faper[1] - 1, faper[0]), 'yyyy-MM-dd'))
         }
         SwalCarga();
         this.casoService.UpdateClaim(siniestroBM).subscribe(
           res => {
             Swal.close();
-            if(res.Message !="Ok"){
-              Swal.fire('Información', res.Message,'warning');
+            if (res.Message != "Ok") {
+              Swal.fire('Información', res.Message, 'warning');
               return;
-            }else{
+            } else {
               Swal.fire({
                 title: 'Información',
                 text: 'Siniestro actualizado correctamente',
@@ -287,15 +309,15 @@ export class FormSiniestroComponent implements OnInit {
           },
           err => {
             Swal.close();
-            Swal.fire('Error',err,'error')
+            Swal.fire('Error', err, 'error')
           }
         )
       }
     }
   }
 
-  disabledForm(){
-    if(this.estadoForm == 3){
+  disabledForm() {
+    if (this.estadoForm == 3) {
       this.form.controls['dFecDenuncia'].disable();
       this.form.controls['sHoraRecepcion'].disable();
       this.form.controls['dFecApertura'].disable();
@@ -306,7 +328,7 @@ export class FormSiniestroComponent implements OnInit {
       this.form.controls['sEquivSiniestro'].disable();
     }
 
-    if(this.estadoForm == 1){
+    if (this.estadoForm == 1) {
       this.form.controls['dFecDenuncia'].disable();
       this.form.controls['sHoraRecepcion'].disable();
     }
@@ -340,6 +362,33 @@ export class FormSiniestroComponent implements OnInit {
 
         }
       })
+    }
+  }
+
+  
+
+  changeDenuncia(){
+    //console.log(this.form.controls['dFecDenuncia'].value);
+    let fecha = this.form.controls['dFecDenuncia'].value;
+    if (fecha != '' || fecha != null) {
+      //let date = (this.form.controls['dFecDenuncia'].value).split('-');
+      let data = new SiniestroBM();
+      data.dFecDenuncia = this.form.controls['dFecDenuncia'].value;
+      data.nCaso = this.casoBM.nCaso;
+      this.casoService.ValidateClaim(data).subscribe(res => {
+        if(res.Message != 'Ok'){
+          Swal.fire('Información',res.Message,'warning');
+          return;
+        }
+      })
+    }
+    //this.form.controls['dFecDenuncia'].setValue('');
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Tab' && (this.form.controls['dFecDenuncia'].value != '' || this.form.controls['dFecDenuncia'].value != null)) {
+      event.preventDefault();
+      this.changeDenuncia()
     }
   }
 
