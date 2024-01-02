3cab76b201587456881bd6048237db2f31aea0bd
Ronald0320 <ronald.retamozo@materiagris.pe>
2024-01-02 10:12:23 -0500

    configuraciones produccion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,8 @@ export class AppConfig{
     public static get URL_SINIESTROS_WEB() : string {
         //return 'http://190.216.170.173/siniestrodes' //DESARROLLO
         //return 'http://190.216.170.173/siniestroqa' //QA
-        return 'http://172.23.10.23/siniestroqa' //QA
+        //return 'http://172.23.10.23/siniestroqa' //QA
         //return 'http://localhost:55556';
+        return 'https://apps.protectasecurity.pe/Siniestros' //PRODUCCION
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
   //reservaApi: 'http://localhost:30896/Api',
   //siniestroAPI: 'http://localhost:30897/Api'
   //siniestroAPI: 'https://plataformadigital.stg.protectasecurity.pe/ApiSiniestroSoatQA/Api'  // QA
-  siniestroAPI: 'http://172.23.10.23/ApiSiniestroSoatQA/Api' //Nuevo_QA
+  //siniestroAPI: 'http://172.23.10.23/ApiSiniestroSoatQA/Api' //Nuevo_QA  https://apps.protectasecurity.pe/ApiSiniestroSOAT/
+  siniestroAPI: 'https://apps.protectasecurity.pe/ApiSiniestroSOAT/Api' // PRODUCCION
   //siniestroAPI: 'https://plataformadigital.stg.protectasecurity.pe/SiniestroSoatApi/Api'    // DESARROLLO
   //siniestroAPI: 'https://plataformadigital.stg.protectasecurity.pe/siniestrosDesaBack/Api'  // CARGA MASIVA
   //http://172.23.2.118/ApiSiniestroSoatQA/
